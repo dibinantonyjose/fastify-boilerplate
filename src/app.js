11f4bcb9ac63524f2c44bcc232f2b3015bec12da
dibinjose <dibinantonyjose@gmail.com>
2021-08-08 23:19:14 +0530

    Connect to mongodb

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 'use strict'
-
-// read .env files
+// require external modules
 require('dotenv').config()
-
-// require the framework
 const Fastify = require('fastify')({ logger: true })
-
-// Require external modules
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
 const oas = require('fastify-oas')
 
+// register fastify ecosystem plugins
 Fastify.register(oas)
 
+// register custom plugins
+Fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'plugins')
+})
+
+Fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'services'),
+  options: Object.assign({ prefix: '/api' })
+})
+
+// start server
 Fastify.listen(process.env.port || 3000, process.env.host || '0.0.0.0', err => {
   if (err) {
     Fastify.log.error(err)
---
+++ src/plugins/mongo.js
@@ -0,0 +1,20 @@
+const fastifyPlugin = require('fastify-plugin')
+const mongoose = require('mongoose')
+
+module.exports = fastifyPlugin(async () => {
+  try {
+    const url = process.env.mongoURI
+    if (url) {
+      await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      })
+      console.log('Database is connected')
+    } else {
+      console.log('Error connecting database')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
