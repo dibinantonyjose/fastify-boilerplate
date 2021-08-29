40f5560c44c40bf13abd793e741b78eff5ef8bb7
dibinjose <dibinantonyjose@gmail.com>
2021-08-29 19:35:36 +0530

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    jsxBracketSameLine: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    spaceBeforeFunctionParen: false
+}
